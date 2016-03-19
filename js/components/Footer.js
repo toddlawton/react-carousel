f7fc259715261fefe3238fbbc71938b1a98f4609
Todd Beauchamp <toddbee@gmail.com>
2016-03-19 18:25:17 -0400

    Fixed heroku demo server

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer>I need some sleep.</footer>
+            <footer></footer>
         );
     }
 }
