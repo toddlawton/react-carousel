e02150512163a3d816b0b4d5c4c3d433f7f29829
Todd Beauchamp <toddbee@gmail.com>
2016-03-19 18:38:30 -0400

    Fixed heroku demo server

--- server.js
+++ server.js
@@ -1,46 +1,20 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
+var express = require('express');
+var app = express();
 
-function send404(response) {
-  response.writeHead(404, {"Content-type" : "text/plain"});
-  response.write("Error 404: resource not found");
-  response.end();
-}
+app.set('port', (process.env.PORT || 5000));
 
-function sendPage(response, filePath, fileContents) {
-  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
-  response.end(fileContents);
-}
+app.use(express.static(__dirname));
 
-function serverWorking(response, absPath) {
-  fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-        if (err) {
-          send404(response)
-        } else {
-          sendPage(response, absPath, data);
-        }
-      });
-    } else {
-      send404(response);
-    }
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  app.get('*', function(req, res){
+    res.sendfile(__dirname + '/index.html');
   });
-}
-
-var server = http.createServer(function(request, response) {
-  var filePath = false;
-
-  if (request.url == '/') {
-    filePath = "index.html";
-  } else {
-    filePath = request.url;
-  }
-
-  var absPath = "./" + filePath;
-  serverWorking(response, absPath);
 });
 
-var port_number = server.listen(process.env.PORT || 80);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
